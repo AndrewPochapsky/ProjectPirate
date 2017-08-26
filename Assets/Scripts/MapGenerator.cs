43e0c6ccca7d982fce088d685e114d482d5ac27d
AndrewPochapsky <andrew.pochapsky@gmail.com>
2017-08-26 13:23:57 -0400

    Colour regions now work, Changed setting in unity

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
+    public enum DrawMode { NoiseMap, ColourMap };
+    public DrawMode drawMode;
+
     [SerializeField]
     private int mapWidth;
     [SerializeField]
     private int mapHeight;
     [SerializeField]
     private float noiseScale;
+
+    [SerializeField]
+    private int octaves;
+    [SerializeField][Range(0,1)]
+    private float persistance;
+    [SerializeField]
+    private float lacunarity;
+
+    [SerializeField]
+    private int seed;
+    [SerializeField]
+    private Vector2 offset;
+
+    public TerrainType[] regions;
     
     [SerializeField]
     private bool autoUpdate;
@@ -24,10 +41,67 @@ public class MapGenerator : MonoBehaviour {
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if(drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+
+        
     }
 
+    private void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if(octaves < 0)
+        {
+            octaves = 0;
+        }
+
+    }
+
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
